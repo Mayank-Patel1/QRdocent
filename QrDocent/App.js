3ea967d74548af0061f70a5ae5af8106275da2f6
danteedmonson <danteedmonson@gmail.com>
2021-08-12 23:18:26 -0400

    basic Hello World mobile app

---
+++ QrDocent/App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  Image,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+const App = () => {
+
+  return (
+    <View style={{backgroundColor:"#282B33", flex:1, justifyContent:"center"}}>
+    <View style={{ flex:0.3, justifyContent:"space-between", alignItems:"center"}}>
+      <Text style={{color:"white",  fontFamily:"Roboto", fontSize:37}}>HELLO WORLD</Text>
+      <Image
+        source={require('./images/image4.png')}
+
+      ></Image>
+</View>
+    </View>
+  );
+};
+
+
+export default App;
